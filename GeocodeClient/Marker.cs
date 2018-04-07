c415d804850fe5b5c9d69a881c36542ed5eac5be
Francis, Tim <timothy.s.francis@gmail.com>
2018-04-06 20:36:01 -0700

    Added a constructor to the Marker class

--- GeocodeClient/Marker.cs
+++ GeocodeClient/Marker.cs
@@ -4,10 +4,21 @@ using System.Text;
 
 /// <summary>
 /// This class will represent a marker on a Google Map
+/// Read more about Markers in the Google Maps JS API:
+/// https://developers.google.com/maps/documentation/javascript/examples/marker-simple
 /// </summary>
 namespace Geocode
 {
     class Marker
     {
+        private float _latitude;
+        private float _longitude;
+
+        public Marker(float lat, float longi)
+        {
+            _latitude = lat;
+            _longitude = longi;
+        }
+
     }
 }
